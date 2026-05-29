4f25cc74ebbf4d11844fc70d62fd49be4d8a3b32
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-05-29 16:59:00 +0300

    main => _

--- apps/client/src/core/lib/etc/timer.ts
+++ apps/client/src/core/lib/etc/timer.ts
@@ -1,6 +1,6 @@
 import { TimerIdT } from '../../../common/types/etc';
 
-export class LibEtc {
+export class LibTimer {
   public static clearTmr(timerRef: TimerIdT): null {
     if (timerRef) clearTimeout(timerRef);
     return null;
