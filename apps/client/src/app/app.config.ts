ed8c6c2ce841583680c92733730492fa86b11de1
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-05-29 19:23:49 +0300

    main => _

--- apps/client/src/app/app.config.ts
+++ apps/client/src/app/app.config.ts
@@ -1,13 +1,25 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideStore } from '@ngrx/store';
+import { rootReducer } from '../store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideStore(rootReducer),
+    provideHttpClient(withFetch()),
+    provideStoreDevtools({ maxAge: 25 }),
+  ],
 };
