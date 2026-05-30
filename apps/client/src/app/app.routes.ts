692fab63d793269f9988df65816dd78978b77ff9
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-05-30 10:02:49 +0300

    main => _

--- apps/client/src/app/app.routes.ts
+++ apps/client/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomePage } from '../pages/homePage/home-page';
+import { NoticePage } from '../pages/noticePage/notice-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePage,
+  },
+  {
+    path: 'notice',
+    component: NoticePage,
+  },
+];
---
+++ apps/client/src/pages/noticePage/notice-page.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notice-page',
+  imports: [],
+  templateUrl: './notice-page.html',
+  styleUrl: './notice-page.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NoticePage {}
