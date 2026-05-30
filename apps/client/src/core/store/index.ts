bdf1841e68d61d192a9f6078d0225ba6aaf3b284
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-05-30 09:23:58 +0300

    main => _

--- apps/client/src/core/store/index.ts
+++ apps/client/src/core/store/index.ts
@@ -1,5 +1,10 @@
 import { ActionReducerMap } from '@ngrx/store';
+import { noticeReducer, NoticeStateT } from '../../features/notice/reducer/reducer';
 
-export interface StoreStateT {}
+export interface StoreStateT {
+  notice: NoticeStateT;
+}
 
-export const rootReducer: ActionReducerMap<StoreStateT> = {};
+export const rootReducer: ActionReducerMap<StoreStateT> = {
+  notice: noticeReducer,
+};
