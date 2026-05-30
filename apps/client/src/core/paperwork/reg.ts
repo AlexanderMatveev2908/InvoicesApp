c91f3b5cf7dca8ce5a61dcb1aecbb600a6e86322
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-05-30 11:40:44 +0300

    main => _

--- apps/client/src/core/paperwork/reg.ts
+++ apps/client/src/core/paperwork/reg.ts
@@ -2,11 +2,6 @@ import { OrNone } from '../../common/types/general';
 import { LibShape } from '../lib/data_structures/shape';
 
 export class Reg {
-  public static readonly TXT: RegExp =
-    /^[\p{L}\p{N}\s'.?!;,"\-\p{Emoji}\p{Extended_Pictographic}\p{Emoji}]*$/u;
-
-  public static readonly EMOJI: RegExp =
-    /^\s*(\p{Extended_Pictographic}|\p{Emoji_Presentation}|\p{Emoji})/u;
   public static readonly INT: RegExp = /^\d+$/;
 
   public static check(arg: OrNone<string>, reg: RegExp): boolean {
