43956c4084a3d56a66cb31518a458c2bc29948c0
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-05-31 13:05:53 +0300

    main => added lib api_args

---
+++ apps/client/src/common/types/api.ts
@@ -0,0 +1,25 @@
+import { Nullable } from './general';
+
+export interface OptToastApiT {
+  toastOk: boolean;
+  toastErr: boolean;
+  okMsg: Nullable<string>;
+}
+
+export interface OptErrApiT {
+  pushOnErr: boolean;
+  pushOnStatus: number[];
+}
+
+export enum ApiStatusT {
+  OK = 200,
+  CREATED = 201,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+  CONFLICT = 409,
+  ENTITY_UNPROCESSABLE = 422,
+  TOO_MANY_REQUESTS = 429,
+  INTERNAL_SERVER_ERROR = 500,
+}
