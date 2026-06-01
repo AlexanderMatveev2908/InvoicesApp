c9a770429f3edfb6bc286e7f0e6e8eb5f75a921d
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-06-01 12:15:46 +0300

    main => _

--- apps/client/src/layout/wake_up/wake-up.ts
+++ apps/client/src/layout/wake_up/wake-up.ts
@@ -1,11 +1,12 @@
 import { UseApiTrackerHk } from '@/core/hooks/use_api_tracker';
 import { WakeUpApiSvc } from '@/features/wake_up/api';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, Signal } from '@angular/core';
 import { BgBlack } from '../bg_black/bg-black';
+import { SpinBtn } from '@/common/components/spins/spin_btn/spin-btn';
 
 @Component({
   selector: 'app-wake-up',
-  imports: [BgBlack],
+  imports: [BgBlack, SpinBtn],
   providers: [UseApiTrackerHk],
   templateUrl: './wake-up.html',
   styleUrl: './wake-up.scss',
@@ -18,4 +19,6 @@ export class WakeUp {
   ngAfterViewInit(): void {
     this.useApiTracker.track(this.wakeUpApi.wakeUp()).subscribe();
   }
+
+  public readonly isBlack: Signal<boolean> = computed(() => this.useApiTracker.isPending());
 }
