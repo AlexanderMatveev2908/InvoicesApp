b29e73db7991861b696790a4623ce8a5763ea7d7
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-06-01 09:22:33 +0300

    main => _

--- apps/client/src/common/components/spins/spin_page_ssr/spin-page-ssr.ts
+++ apps/client/src/common/components/spins/spin_page_ssr/spin-page-ssr.ts
@@ -1,4 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AppEventT } from '@/common/types/general';
+import { LibMetaEvent } from '@/core/lib/meta_event';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+  InputSignal,
+  Signal,
+} from '@angular/core';
+import { v4 } from 'uuid';
 
 @Component({
   selector: 'app-spin-page-ssr',
@@ -7,4 +17,21 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './spin-page-ssr.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SpinPageSsr {}
+export class SpinPageSsr {
+  public readonly eventT: InputSignal<AppEventT> = input.required();
+
+  public readonly IDs: string[] = Array.from({ length: 10 }, () => v4());
+
+  private readonly dotsCount: number = this.IDs.length;
+
+  public mainColor: Signal<string> = computed(() => LibMetaEvent.cssVarByT(this.eventT()));
+
+  // ? derived
+  public getRotation(idx: number): string {
+    return `${(360 / this.dotsCount) * idx}deg`;
+  }
+
+  public getDelay(idx: number): string {
+    return `${(1 / this.dotsCount) * idx}s`;
+  }
+}
--- apps/client/src/pages/home_page/home-page.ts
+++ apps/client/src/pages/home_page/home-page.ts
@@ -1,3 +1,4 @@
+import { UseNavSvc } from '@/core/services/use_nav';
 import { ToastSlice } from '@/features/toast/slice';
 import { AfterViewInit, ChangeDetectionStrategy, Component, inject } from '@angular/core';
 
@@ -9,15 +10,24 @@ import { AfterViewInit, ChangeDetectionStrategy, Component, inject } from '@angu
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePage implements AfterViewInit {
+  private readonly useNav: UseNavSvc = inject(UseNavSvc);
   private readonly toastSlice: ToastSlice = inject(ToastSlice);
 
   ngAfterViewInit(): void {
     setTimeout(() => {
+      this.useNav.pushNotice({
+        cb: null,
+        tmpt: 'MAIL',
+        eventT: 'WARN',
+        msg: 'banana',
+        status: 300,
+      });
+
       this.toastSlice.openToast({
         eventT: 'WARN',
         msg: 'banana',
         status: 300,
       });
-    }, 2000);
+    }, 1500);
   }
 }
