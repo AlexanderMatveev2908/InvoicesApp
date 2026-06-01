2c65281aa6b5cc8d6c1d84666f23d09fff2b768d
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-06-01 18:31:49 +0300

    variables_css_and_tailwind => added league font

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        danger: '#dc2626',
+        white_0: 'F8F8FB',
+        black_0: '#141625',
+        red_0: '#9277FF',
+        red_1: '#EC5757',
+        gray_0: '#DFE3FA',
+        gray_1: '#888EB0',
+        gray_2: '#7E88C3',
+        gray_3: '#0C0E16',
+        blue_0: '#7C5DFA',
+        blue_1: '#9277FF',
+        blue_2: '#1E2139',
+        blue_3: '#252945',
       },
     },
   },
