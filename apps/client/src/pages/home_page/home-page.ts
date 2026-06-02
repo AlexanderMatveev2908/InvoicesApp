6f4144693d001515b5c12d08bd50e3e7d1b8d35f
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-06-02 10:25:27 +0300

    home_page_mobile_light => made header home a component

--- apps/client/src/pages/home_page/home-page.ts
+++ apps/client/src/pages/home_page/home-page.ts
@@ -1,23 +1,11 @@
-import { SvgFillArrowDown } from '@/common/components/svgs/fill/arrow_down/arrow-down';
-import { SvgFillPlus } from '@/common/components/svgs/fill/plus/plus';
-import { SvgT } from '@/common/types/general';
-import { InvoiceT } from '@/common/types/invoices';
-import { UseThemeSvc } from '@/core/services/use_theme';
-import { mockInvoices } from '@/mock/data';
-import { NgClass, NgComponentOutlet } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { HeaderHomeMobile } from '@/common/components/mobile/header_home_mobile/header-home-mobile';
 
 @Component({
   selector: 'app-home-page',
-  imports: [NgClass, NgComponentOutlet],
+  imports: [HeaderHomeMobile],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomePage {
-  public readonly useTheme: UseThemeSvc = inject(UseThemeSvc);
-  public readonly mockInvoices: InvoiceT[] = mockInvoices;
-
-  public readonly SvgArrowDown: SvgT = SvgFillArrowDown;
-  public readonly SvgPlus: SvgT = SvgFillPlus;
-}
+export class HomePage {}
