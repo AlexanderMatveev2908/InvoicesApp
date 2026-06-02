e58c4625ea00787c9855b382643330505f5150c0
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-06-02 13:53:45 +0300

    navbar_tablet => added add invoice button for tablet

--- apps/client/src/common/components/header_home_mobile_tablet/header-home-mobile-tablet.ts
+++ apps/client/src/common/components/header_home_mobile_tablet/header-home-mobile-tablet.ts
@@ -2,22 +2,23 @@ import { InvoiceT } from '@/common/types/invoices';
 import { UseThemeSvc } from '@/core/services/use_theme';
 import { mockInvoices } from '@/mock/data';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { SvgFillArrowDown } from '../../svgs/fill/arrow_down/arrow-down';
-import { SvgFillPlus } from '../../svgs/fill/plus/plus';
 import { SvgT } from '@/common/types/general';
 import { NgClass, NgComponentOutlet } from '@angular/common';
+import { SvgFillArrowDown } from '../svgs/fill/arrow_down/arrow-down';
+import { SvgFillPlus } from '../svgs/fill/plus/plus';
+import { AddInvoiceMobile } from '../mobile/add_invoice_mobile/add-invoice-mobile';
+import { AddInvoiceTablet } from '../tablet/add_invoice_tablet/add-invoice-tablet';
 
 @Component({
-  selector: 'app-header-home-mobile',
-  imports: [NgClass, NgComponentOutlet],
-  templateUrl: './header-home-mobile.html',
-  styleUrl: './header-home-mobile.scss',
+  selector: 'app-header-home-mobile-tablet',
+  imports: [NgClass, NgComponentOutlet, AddInvoiceMobile, AddInvoiceTablet],
+  templateUrl: './header-home-mobile-tablet.html',
+  styleUrl: './header-home-mobile-tablet.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderHomeMobile {
+export class HeaderHomeMobileTablet {
   public readonly useTheme: UseThemeSvc = inject(UseThemeSvc);
   public readonly appMockInvoices: InvoiceT[] = mockInvoices;
 
   public readonly SvgArrowDown: SvgT = SvgFillArrowDown;
-  public readonly SvgPlus: SvgT = SvgFillPlus;
 }
