a3c7a83055cd49915db6884b332776cb84c03190
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-06-02 13:05:26 +0300

    home_page_mobile_light => added no invoice section

--- apps/client/src/pages/home_page/home-page.ts
+++ apps/client/src/pages/home_page/home-page.ts
@@ -3,14 +3,18 @@ import { HeaderHomeMobile } from '@/common/components/mobile/header_home_mobile/
 import { InvoicesHomeMobile } from '@/common/components/mobile/invoices_home_mobile/invoices-home-mobile';
 import { NgClass } from '@angular/common';
 import { UseThemeSvc } from '@/core/services/use_theme';
+import { InvoiceT } from '@/common/types/invoices';
+import { mockInvoices } from '@/mock/data';
+import { NoInvoicesMobile } from '@/common/components/mobile/no_invoices_mobile/no-invoices-mobile';
 
 @Component({
   selector: 'app-home-page',
-  imports: [HeaderHomeMobile, InvoicesHomeMobile, NgClass],
+  imports: [HeaderHomeMobile, InvoicesHomeMobile, NgClass, NoInvoicesMobile],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePage {
   public readonly useTheme: UseThemeSvc = inject(UseThemeSvc);
+  public readonly appMockInvoices: InvoiceT[] = mockInvoices;
 }
