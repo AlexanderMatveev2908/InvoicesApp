60a445232617f98c234f7f1ae604c6c6d0ea320c
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-06-02 08:43:24 +0300

    home_page_mobile_light => made app reload curr theme saved in storage on mount

--- apps/client/src/app/app.ts
+++ apps/client/src/app/app.ts
@@ -1,7 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { AfterViewInit, Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Toast } from '@/layout/toast/toast';
 import { WakeUp } from '@/layout/wake_up/wake-up';
+import { ThemeSlice } from '@/features/theme/slice';
+import { UseStorageSvc } from '@/core/services/use_storage';
+import { UsePlatformSvc } from '@/core/services/use_platform';
+import { ThemeT } from '@/features/theme/reducer';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +13,18 @@ import { WakeUp } from '@/layout/wake_up/wake-up';
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements AfterViewInit {
+  private readonly themeSlice: ThemeSlice = inject(ThemeSlice);
+  private readonly useStorage: UseStorageSvc = inject(UseStorageSvc);
+  private readonly usePlatform: UsePlatformSvc = inject(UsePlatformSvc);
+
+  ngAfterViewInit(): void {
+    if (this.usePlatform.isServer) return;
+
+    const theme = this.useStorage.getItem('theme');
+    if (!theme) return;
+
+    if (!['light', 'dark'].includes(theme as ThemeT)) return;
+    this.themeSlice.setTheme(theme as ThemeT);
+  }
 }
