fa4fdc8fe7cf0fe06089e0b6539ba43b0f4dc84b
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-06-03 09:45:51 +0300

    invoice_page => _

---
+++ apps/client/src/core/lib/data_structures/format.ts
@@ -0,0 +1,9 @@
+export class LibFormat {
+  public static formatDate(date: string): string {
+    return new Intl.DateTimeFormat('en-GB', {
+      day: '2-digit',
+      month: 'short',
+      year: 'numeric',
+    }).format(new Date(date));
+  }
+}
--- apps/client/src/pages/invoice_page/invoice-page.ts
+++ apps/client/src/pages/invoice_page/invoice-page.ts
@@ -2,6 +2,7 @@ import { SvgAdvArrowLeft } from '@/common/components/svgs/advanced/arrow_left/ar
 import { Optional, SvgT } from '@/common/types/general';
 import { InvoiceT } from '@/common/types/invoices';
 import { UseMetaStatusDir } from '@/core/directives/use_meta_status_dir';
+import { LibFormat } from '@/core/lib/data_structures/format';
 import { LibLog } from '@/core/lib/log';
 import { UseNavSvc } from '@/core/services/use_nav';
 import { mockInvoices } from '@/mock/data';
@@ -31,6 +32,10 @@ export class InvoicePage extends UseMetaStatusDir implements OnInit {
   public readonly currInvoice: Signal<Optional<InvoiceT>> = computed(() =>
     mockInvoices.find((el: InvoiceT) => el.id === this.useNav.pathVariables()?.['invoiceID']),
   );
+
+  public formatDate(): string {
+    return LibFormat.formatDate(this.currInvoice()?.date!);
+  }
 
   ngOnInit(): void {
     LibLog.main('invoice page', this.getTxtClrByStatus(this.currInvoice()?.status!));
