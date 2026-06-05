3dbe86c3f00d54c2d0120fc52d3aa59c3cfa2787
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-06-05 07:22:24 +0300

    main => _

--- apps/client/src/pages/invoices_post_page/invoices-post-page.ts
+++ apps/client/src/pages/invoices_post_page/invoices-post-page.ts
@@ -14,6 +14,9 @@ import { InvoicesFormMobile } from '@/features/invoices/forms/invoices_form_mobi
 import { UseInvoicesApiSvc } from '@/features/invoices/api';
 import { UseApiTrackerHk } from '@/core/hooks/use_api_tracker';
 import { InvoiceFormT } from '@/features/invoices/paperwork/InvoiceFormMng';
+import { InvoicesSlice } from '@/features/invoices/slice';
+import { UseNavSvc } from '@/core/services/use_nav';
+import { switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-invoices-post-page',
@@ -26,16 +29,37 @@ import { InvoiceFormT } from '@/features/invoices/paperwork/InvoiceFormMng';
 export class InvoicesPostPage {
   public readonly useTheme: UseThemeSvc = inject(UseThemeSvc);
 
+  private readonly invoicesSlice: InvoicesSlice = inject(InvoicesSlice);
+  private readonly useNav: UseNavSvc = inject(UseNavSvc);
+
   public readonly useInvoicesApi: UseInvoicesApiSvc = inject(UseInvoicesApiSvc);
 
   public readonly saveTracker = new UseApiTrackerHk();
   public readonly draftTracker = new UseApiTrackerHk();
 
   public readonly submitSave = (data: InvoiceFormT): void => {
-    this.saveTracker.track(this.useInvoicesApi.savePendingInvoice(data)).subscribe();
+    this.saveTracker
+      .track(
+        this.useInvoicesApi.savePendingInvoice(data).pipe(
+          tap(() => {
+            this.invoicesSlice.refreshKey();
+            this.useNav.replace('/', { from: null });
+          }),
+        ),
+      )
+      .subscribe();
   };
 
   public readonly submitDraft = (data: InvoiceFormT): void => {
-    this.draftTracker.track(this.useInvoicesApi.saveDraftInvoice(data)).subscribe();
+    this.draftTracker
+      .track(
+        this.useInvoicesApi.saveDraftInvoice(data).pipe(
+          tap(() => {
+            this.invoicesSlice.refreshKey();
+            this.useNav.replace('/', { from: null });
+          }),
+        ),
+      )
+      .subscribe();
   };
 }
