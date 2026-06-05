391c842b399482906d85d79652d4842ad00b4423
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-06-05 09:26:31 +0300

    filter_btn => _

--- apps/client/src/common/components/mobile/filter_invoices_mobile/filter-invoices-mobile.ts
+++ apps/client/src/common/components/mobile/filter_invoices_mobile/filter-invoices-mobile.ts
@@ -3,6 +3,7 @@ import { NgClass, NgComponentOutlet } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { UseThemeSvc } from '@/core/services/use_theme';
 import { SvgAdvArrowDown } from '../../svgs/advanced/arrow_down/arrow-down';
+import { InvoiceStatusT } from '@/common/types/invoices';
 
 @Component({
   selector: 'app-filter-invoices-mobile',
@@ -14,4 +15,6 @@ import { SvgAdvArrowDown } from '../../svgs/advanced/arrow_down/arrow-down';
 export class FilterInvoicesMobile {
   public readonly SvgArrowDown: SvgT = SvgAdvArrowDown;
   public readonly useTheme: UseThemeSvc = inject(UseThemeSvc);
+
+  public readonly options: InvoiceStatusT[] = ['DRAFT', 'PENDING', 'PAID'];
 }
