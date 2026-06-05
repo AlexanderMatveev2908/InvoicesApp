691f2c726ad20b3d2b09a805cd9699977e1ebd61
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-06-05 13:26:46 +0300

    post_invoice_tablet => _

--- apps/client/src/features/invoices/forms/invoices_form_tablet_desktop/invoices-form-tablet-desktop.ts
+++ apps/client/src/features/invoices/forms/invoices_form_tablet_desktop/invoices-form-tablet-desktop.ts
@@ -3,10 +3,14 @@ import { BgBlack } from '@/layout/bg_black/bg-black';
 import { InvoicesSlice } from '../../slice';
 import { UseThemeSvc } from '@/core/services/use_theme';
 import { NgClass } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { InvoiceFormMng } from '../../paperwork/InvoiceFormMng';
+import { InvoicesUiFct } from '../../ui_factory';
+import { TxtFormInput } from '@/common/components/forms/txt_form_input/txt-form-input';
 
 @Component({
   selector: 'app-invoices-form-tablet-desktop',
-  imports: [BgBlack, NgClass],
+  imports: [BgBlack, NgClass, ReactiveFormsModule, TxtFormInput],
   templateUrl: './invoices-form-tablet-desktop.html',
   styleUrl: './invoices-form-tablet-desktop.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -19,4 +23,15 @@ export class InvoicesFormTabletDesktop {
   );
 
   public readonly useTheme: UseThemeSvc = inject(UseThemeSvc);
+  public readonly InvoicesUiFct = InvoicesUiFct;
+
+  public readonly form: FormGroup = InvoiceFormMng.form();
+
+  public getCtrl(name: string): FormControl {
+    return this.form.get(name) as FormControl;
+  }
+
+  public getItemCtrl(index: number, key: 'name' | 'qty' | 'price'): FormControl {
+    return this.form.get(`itemsList.${index}.${key}`) as FormControl;
+  }
 }
